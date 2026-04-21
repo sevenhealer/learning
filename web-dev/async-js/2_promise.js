41d3b6bfea145b5bd1cbecb2997def23f5186aeb
iamrohanchatterjee <rohan.chat2003@gmail.com>
2026-04-21 16:07:46 +0530

    Excalidraw Updated

--- web-dev/async-js/2_promise.js
+++ web-dev/async-js/2_promise.js
@@ -11,3 +11,44 @@ function setTimeoutPromisified(ms){
 }
 
 setTimeoutPromisified(2000).then(fun)
+
+//simplified promise
+function simplifiedPromise(){
+    const p = new Promise(function(resolve){
+        setTimeout(function(){
+            resolve();
+        }, 3000)
+    })
+    return p;
+}
+
+console.log(simplifiedPromise())
+
+simplifiedPromise().then(function(){
+    console.log("After 3sec using simplified promise")
+})
+
+//own promise class
+class Promise2{
+    constructor(fun){
+        const onDone = () => {
+            this.resolve()
+        }
+        fun(onDone);
+    }
+    then(resolve){
+        this.resolve = resolve;
+    }
+}
+
+function ownPromisifiedsetTimeout(){
+    return new Promise2((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, 4000)
+    })
+}
+
+ownPromisifiedsetTimeout().then(() => {
+    console.log("Own Promise Called")
+})
--- web-dev/async-js/Readme.md
+++ web-dev/async-js/Readme.md
@@ -6,3 +6,5 @@ create promisified function of
     fs.readFile
 
 Slide: https://excalidraw.com/#room=82024daf0fda8055094b,hF6l6jw-lkJsuIyBFRvjjQ
+
+read-only: https://excalidraw.com/#room=82024daf0fda8055094b,hF6l6jw-lkJsuIyBFRvjjQ
